4aa4f2feb864f9c16c93f5980b642005bd1d3783
Kuatacho <107372118+Kuatacho@users.noreply.github.com>
2022-06-13 00:54:53 -0400

    Seguir

---
+++ productos.php
@@ -0,0 +1,49 @@
+<?php include ("template/cabecera.php");?>
+
+<div class="col-md-3">
+    <div class="card">
+        
+            <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/User_icon-cp.svg/1200px-User_icon-cp.svg.png" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Libro PHP</h4>
+                    <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver Mas</a>
+                </div>
+    </div>     
+</div>
+
+<div class="col-md-3">
+    <div class="card">
+        
+            <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/User_icon-cp.svg/1200px-User_icon-cp.svg.png" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Libro PHP</h4>
+                    <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver Mas</a>
+                </div>
+    </div>     
+</div>
+
+<div class="col-md-3">
+    <div class="card">
+        
+            <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/User_icon-cp.svg/1200px-User_icon-cp.svg.png" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Libro PHP</h4>
+                    <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver Mas</a>
+                </div>
+    </div>     
+</div>
+
+<div class="col-md-3">
+    <div class="card">
+        
+            <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/User_icon-cp.svg/1200px-User_icon-cp.svg.png" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Libro PHP</h4>
+                    <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver Mas</a>
+                </div>
+    </div>     
+</div>
+
+
+
+<?php include("template/pie.php"); ?>
