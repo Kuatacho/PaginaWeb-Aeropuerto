10de1ea73cc0805715ae14fb891b9f58bf420858
Kuatacho <107372118+Kuatacho@users.noreply.github.com>
2022-06-13 01:38:10 -0400

    PostFunca
    Funciona el metodo post

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -1,68 +1,72 @@
 <?php include("../template/cabecera.php"); ?>
-
-<?php 
-    print_r($_POST); 
+<?php
+print_r($_POST);
+print_r($_FILES);
 ?>
 
 <div class="col-md-5">
-    
     <div class="card">
-
         <div class="card-header">
-            Datos De Libro
+            Datos Libro
         </div>
 
         <div class="card-body">
+
             <form method="POST" enctype="multipart/form-data">
 
-            <div class = "form-group">
-            <label for="txtID">ID:</label>
-            <input type="text" class="form-control" name="txtID" id="txtID" placeholder="ID">
-            </div>
+                <div class = "form-group">
+                <label for="txtID">ID</label>
+                <input type="text" class="form-control" name="txtID" id="txtID" placeholder="ID">
+                </div>
 
-            <div class = "form-group">
-            <label for="txtNombre">Nombre:</label>
-            <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre Del Libro">
-            </div>
+                <div class = "form-group">
+                <label for="txtNombre">Nombre</label>
+                <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre Del Libro">
+                </div>
 
-            <div class = "form-group">
-            <label for="txtNombre">Imagen:</label>
-            <input type="file" class="form-control" name="txtImagen" id="txtImagen" placeholder="Nombre">
-            </div>
+                <div class = "form-group">
+                <label for="txtID">Imagen</label>
+                <input type="file" class="form-control" name="txtImagen" id="txtImagen" placeholder="ID">
+                </div>
 
-            <div class="btn-group" role="group" aria-label="">
-                <button type="button" name="accion" value="Agregar" class="btn btn-success">Agregar</button>
-                <button type="button" name="accion" value="Modificar" class="btn btn-warning">Modificar</button>
-                <button type="button" name="accion" value="Cancelar"  class="btn btn-info">Cancelar</button>
-            </div>
-
-            </form>               
+                <div class="btn-group" role="group" aria-label="">
+                <button type="submit" name="accion" value="Agregar" class="btn btn-success">Agregar</button>
+                <button type="submit" name="accion"value="Modificar" class="btn btn-warning">Modificar</button>
+                <button type="submit" name="accion" value="Cancelar"class="btn btn-info">Cancelar</button>
+                </div>
+            </form>
         </div>
+        
     </div>
-</div>
 
+
+
+    
+    
+    
+
+</div>
 <div class="col-md-7">
     <table class="table table-bordered">
         <thead>
             <tr>
                 <th>ID</th>
-                <th>Nombre Del Libro</th>
+                <th>Nombre</th>
                 <th>Imagen</th>
                 <th>Acciones</th>
             </tr>
         </thead>
         <tbody>
-
             <tr>
                 <td>2</td>
-                <td>Aprende PHP</td>
+                <td>Aprende</td>
                 <td>imagen.jpg</td>
-                <td>Seleccionar ├ Borrar</td>
-
+                <td>Seleccionar / Borrar</td>
             </tr>
-            
         </tbody>
     </table>
+    
 </div>
+
 
 <?php include("../template/pie.php") ?>
