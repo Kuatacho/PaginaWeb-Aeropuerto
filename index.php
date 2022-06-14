d630bfdbd17ac99c61d7d3fa77038b8d4f39ac33
Kuatacho <107372118+Kuatacho@users.noreply.github.com>
2022-06-14 00:02:15 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php include ("template/cabecera.php");?>
 
             
-            <div class="jumbotron">
-                <h1 class="display-3">Jumbo heading</h1>
+            <div class="jumbotron text-center">
+                <h1 class="display-3">Bienvenidos al Aeropuerto Internacional De La Paz</h1>
                 <p class="lead">Jumbo helper text</p>
                 <hr class="my-2">
                 <p>More info</p>
