8c26ec57f86919aaa65736341915e1281058a999
Kuatacho <107372118+Kuatacho@users.noreply.github.com>
2022-06-13 23:55:01 -0400

    VideoAcabadov1

--- productos.php
+++ productos.php
@@ -1,48 +1,32 @@
 <?php include ("template/cabecera.php");?>
+<?php 
+include("administrador/config/bd.php");
 
+$sentenciaSQL=$conexion->prepare("SELECT * FROM libros");
+$sentenciaSQL->execute();
+$listaLibros=$sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
+<?php  foreach($listaLibros as $libro){ ?>
 <div class="col-md-3">
     <div class="card">
         
-            <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/User_icon-cp.svg/1200px-User_icon-cp.svg.png" alt="">
+            <img class="card-img-top" src="./img/<?php echo $libro['imagen']; ?>" alt="">
                 <div class="card-body">
-                    <h4 class="card-title">Libro PHP</h4>
+                    <h4 class="card-title"><?php echo $libro['nombre']; ?></h4>
                     <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver Mas</a>
                 </div>
     </div>     
 </div>
+<?php } ?>
 
-<div class="col-md-3">
-    <div class="card">
-        
-            <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/User_icon-cp.svg/1200px-User_icon-cp.svg.png" alt="">
-                <div class="card-body">
-                    <h4 class="card-title">Libro PHP</h4>
-                    <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver Mas</a>
-                </div>
-    </div>     
-</div>
 
-<div class="col-md-3">
-    <div class="card">
-        
-            <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/User_icon-cp.svg/1200px-User_icon-cp.svg.png" alt="">
-                <div class="card-body">
-                    <h4 class="card-title">Libro PHP</h4>
-                    <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver Mas</a>
-                </div>
-    </div>     
-</div>
 
-<div class="col-md-3">
-    <div class="card">
-        
-            <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/User_icon-cp.svg/1200px-User_icon-cp.svg.png" alt="">
-                <div class="card-body">
-                    <h4 class="card-title">Libro PHP</h4>
-                    <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver Mas</a>
-                </div>
-    </div>     
-</div>
+
+
+
+
 
 
 
