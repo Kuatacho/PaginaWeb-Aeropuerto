fd2bbe11dd9d8e0f9a4b235f5c2547abab49964c
Kuatacho <107372118+Kuatacho@users.noreply.github.com>
2022-06-16 21:12:31 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,18 @@
 <?php include ("template/cabecera.php");?>
 
             
-            <div class="jumbotron text-center">
+                    
+                
+            <div class="jumbotron text-center justify-content-center align-item-center ">
                 <h1 class="display-3">Bienvenidos al Aeropuerto Internacional De La Paz</h1>
                 <p class="lead">La Ciudad Maravilla</p>
                 <hr class="my-2">
                 <p>¿Mas Detalles?</p>
-                <p class="lead">
-                    <a class="btn btn-primary btn-lg" href="checkin.php" role="button">Check-in</a>
-                </p>
-                <p class="lead">
-                    <a class="btn btn-primary btn-lg" href="checkinmenores.php" role="button">Check-in Menores de Edad</a>
-                </p>
-                <p class="lead">
-                    <a class="btn btn-primary btn-lg" href="equipaje.php" role="button">Equipaje</a>
-                </p>
-                <p class="lead">
-                    <a class="btn btn-primary btn-lg" href="equipaje.php" role="button">Pet-Check</a>
-                </p>
-                <p class="lead">
-                    <a class="btn btn-primary btn-lg" href="equipaje.php" role="button">Reserva Tu Boleto</a>
-                </p>
+                    
             </div>
+                
+            
+            
 <?php include("template/pie.php"); ?>
 
         
